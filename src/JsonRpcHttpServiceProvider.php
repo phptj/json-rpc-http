0e10bd0f9c3df07d3baaecf458f24fb3269fa051
tangjun <469307838@qq.com>
2020-03-10 15:41:35 +0800

    删除不需要的代码

--- src/JsonRpcHttpServiceProvider.php
+++ src/JsonRpcHttpServiceProvider.php
@@ -14,22 +14,10 @@ class JsonRpcHttpServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         $this->publishes([
             __DIR__.'/config/rpc-services.php' => config_path('rpc-services.php'),
         ], 'config');
 
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->singleton("JsonRpcHttp",function($app){
-            return new \TangJun\SysAuditLog\Repositories\JsonRpcHttpClient;
-        });
-    }
 }
